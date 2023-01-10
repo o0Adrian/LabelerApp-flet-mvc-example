30f813096ee47651695e163746980040d4e6bc2f
Carlos Monroy <carlos.monroy@chromeriver.com>
2023-01-10 02:43:07 -0600

    Updating alert messages

--- mvc/controller.py
+++ mvc/controller.py
@@ -1,4 +1,4 @@
-from flet_mvc import FletController
+from flet_mvc import FletController, alert
 from api import luxmo_api
 import flet as ft
 
@@ -13,7 +13,7 @@ class Controller(FletController):
             self.model.checked_items.set_value(self.model.checked_items() - 1) 
             self.model.files().remove(file)            
             
-    def reload_app(self, e=None, show_snack_bar=True):
+    def reload_app(self, e=None, show_alert=True):
         last_one = self.model.n_invoices()
         self.model.n_invoices.reset()
         self.model.files.reset()
@@ -21,7 +21,7 @@ class Controller(FletController):
         self.model.create_checkboxes()
         self.model.checked_items.reset() 
         
-        if show_snack_bar:
+        if show_alert:
         # Update Msg
             diff = self.model.n_invoices() - last_one
             if diff == 0:
@@ -31,31 +31,17 @@ class Controller(FletController):
             else:
                 update_msg = f"{diff * -1} invoices have been removed." if diff != -1 else "One invoice has been removed"
             
-            self.page.snack_bar = ft.SnackBar(
-                    content=ft.Text(f"The view has been reloaded. {update_msg}"),
-                    bgcolor="blue200"
-            )
-            self.page.snack_bar.open = True
+            self.alert(f"The view has been reloaded. {update_msg}", alert.INFO)
+
         self.update()
         
     def create_labels(self, e):
         # Error handling
         if self.model.checked_items() == 0:
-            self.page.snack_bar = ft.SnackBar(
-                content=ft.Text(f"! [ERROR] No invoice was selected. Please select an invoice and try again."),
-                action="Understood",
-                action_color="blue200"
-                )
-            self.page.snack_bar.open = True
-            self.update()
+            self.alert("! [ERROR] No invoice was selected. Please select an invoice and try again.", alert.ADVICE)
         elif self.model.utility() <= 0 and type(self.model.utility()) != str:
-            self.page.snack_bar = ft.SnackBar(
-                content=ft.Text(f"! [ERROR] - Utility percentage should be greater than 0.0%"),
-                action="Understood",
-                action_color="blue200"
-                )
-            self.page.snack_bar.open = True
-            self.update()
+            self.alert("! [ERROR] - Utility percentage should be greater than 0.0%", alert.ADVICE)
+
         # Creating labels
         else:
             try:
@@ -63,30 +49,14 @@ class Controller(FletController):
                 luxmo_api(self.model.utility(), self.model.files())
                 
                 # Reload view
-                self.reload_app(show_snack_bar=False)
+                self.reload_app(show_alert=False)
                 
                 # Succeed msg
-                self.page.snack_bar = ft.SnackBar(
-                    content=ft.Row(
-                        controls=[
-                            ft.Icon(ft.icons.CHECK_CIRCLE, color="white"),
-                            ft.Text(f"Las labels have been created. The selected invoices\nhave been moved to the 'labeled_invoices' folder"),
-                        ],
-                        # wrap=True,
-                    ),
-                    action="OK",
-                    action_color="black",
-                    bgcolor="green300",
-                )
-                self.page.snack_bar.open = True
-                self.update()
+                self.alert("The labels have been created. The selected invoices\nhave been moved to the 'labeled_invoices' folder", alert.SUCCESS)
+
             except Exception as e:
-                self.page.snack_bar = ft.SnackBar(
-                    content=ft.Text(f"[ERROR] Contact support -- Error found: {e}"),
-                    bgcolor="error",
-                )
-                self.page.snack_bar.open = True
-                self.update()
+                self.alert(f"[ERROR] Contact support -- Error found: {e}")
+
             
     def check_digits_only(self, e):
         try:
